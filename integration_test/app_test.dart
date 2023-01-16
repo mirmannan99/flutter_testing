fb1cb26dc4bfd25e5f5bf0633be76e60b5b73e89
Max Weber <max.weber@sonomotors.com>
2023-01-16 08:09:33 +0100

    Patrol Bootstrap

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,58 +1,46 @@
+// ignore_for_file: avoid_print
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_testing_tutorial/main.dart' as app;
-import 'package:integration_test/integration_test.dart';
+import 'package:patrol/patrol.dart';
+
+import 'config.dart';
+
+// This is an example integration test using Patrol. Use it as a base to
+// create your own Patrol-powered test.
+//
+// To run it, you have to use `patrol drive` instead of `flutter test`.
 
 void main() {
-  group('App Test', () {
-    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-    testWidgets("full app test", (tester) async {
-      app.main();
-      await tester.pumpAndSettle();
-
-      var emailField = find.byType(TextFormField).first;
-      var passwordField = find.byType(TextFormField).last;
-      var loginButton = find.byType(ElevatedButton).first;
-
-      // Enter text for the email address
-      await tester.enterText(emailField, 'flutterexp@gmail.com');
-      await Future.delayed(Duration(seconds: 1));
-
-      // Enter text for the password
-      await tester.enterText(passwordField, '123456');
-      await Future.delayed(Duration(seconds: 1));
-
-      await tester.tap(loginButton);
-      await tester.pumpAndSettle();
-
-      var firstCheckbox = find.byType(Checkbox).first;
-
-      expect(
-        tester.getSemantics(firstCheckbox),
-        matchesSemantics(
-            hasTapAction: true,
-            hasCheckedState: true,
-            isChecked: false,
-            hasEnabledState: true,
-            isEnabled: true,
-            isFocusable: true),
+  patrolTest(
+    'counter state is the same after going to home and switching apps',
+        ($) async {
+      // Replace with your own app widget.
+      await $.pumpWidgetAndSettle(
+        MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(title: const Text('app')),
+            backgroundColor: Colors.blue,
+          ),
+        ),
       );
 
-      await tester.tap(firstCheckbox);
-      await tester.pumpAndSettle();
-      await Future.delayed(Duration(seconds: 1));
+      await $.native.pressHome();
 
-      expect(
-        tester.getSemantics(firstCheckbox),
-        matchesSemantics(
-            hasTapAction: true,
-            hasCheckedState: true,
-            isChecked: true,
-            hasEnabledState: true,
-            isEnabled: true,
-            isFocusable: true),
-      );
-    });
-  });
+      await $.native.pressDoubleRecentApps();
+
+      await $.native.openNotifications();
+
+      await $.native.enableWifi();
+      await $.native.disableWifi();
+      await $.native.enableWifi();
+
+      await $.native.pressBack();
+
+      expect($('app'), findsOneWidget);
+    },
+    config: patrolConfig,
+    nativeAutomatorConfig: nativeAutomatorConfig,
+    nativeAutomation: true,
+
+  );
 }

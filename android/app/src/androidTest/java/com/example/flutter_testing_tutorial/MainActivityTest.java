c7fd2f02228651e640235c8de7dc6c29abb92e87
Max <weber.max.1988@live.de>
2023-01-18 14:18:29 +0100

    Added some additions to make patrol CI ready

---
+++ android/app/src/androidTest/java/com/example/flutter_testing_tutorial/MainActivityTest.java
@@ -0,0 +1,14 @@
+package pl.leancode.patrol.example; // replace with your app's package
+
+import com.example.flutter_testing_tutorial.MainActivity;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import pl.leancode.patrol.PatrolTestRule;
+import pl.leancode.patrol.PatrolTestRunner;
+
+@RunWith(PatrolTestRunner.class)
+public class MainActivityTest {
+    @Rule
+    public PatrolTestRule<MainActivity> rule = new PatrolTestRule<>(MainActivity.class);
+}
